ae913569596f9d545eb535c46f011b76f4b49af1
Zuro <darckProyect8@gmail.com>
2025-09-06 16:17:48 -0500

    Envio de correos funcional, falto arreglar la configuracion del bean de java mail sender

--- src/main/java/com/sanisidro/restaurante/core/email/config/EmailConfig.java
+++ src/main/java/com/sanisidro/restaurante/core/email/config/EmailConfig.java
@@ -1,5 +1,6 @@
 package com.sanisidro.restaurante.core.email.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -10,22 +11,41 @@ import java.util.Properties;
 @Configuration
 public class EmailConfig {
 
+    @Value("${spring.mail.host}")
+    private String host;
+
+    @Value("${spring.mail.port}")
+    private int port;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
+    @Value("${spring.mail.properties.mail.smtp.auth:true}")
+    private boolean auth;
+
+    @Value("${spring.mail.properties.mail.smtp.starttls.enable:true}")
+    private boolean starttls;
+
+    @Value("${spring.mail.properties.mail.debug:false}")
+    private boolean debug;
+
     @Bean
     public JavaMailSender javaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(587);
-
-        mailSender.setUsername("tu_correo@gmail.com");
-        mailSender.setPassword("tu_contraseña");
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.debug", "false");
+        props.put("mail.smtp.auth", String.valueOf(auth));
+        props.put("mail.smtp.starttls.enable", String.valueOf(starttls));
+        props.put("mail.debug", String.valueOf(debug));
 
         return mailSender;
     }
-
 }
